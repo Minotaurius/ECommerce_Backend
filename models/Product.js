775043bcd056d0faddbb621ae84c7dc4cacf74c3
Minotaurius <brandonslanger@gmail.com>
2022-08-18 16:37:39 -0500

    Began working on models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,26 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // need something else here need to come back...foreign key? belongs to category??
+    }
   },
   {
     sequelize,
