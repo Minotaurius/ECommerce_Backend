5676946514cfd9a3333c7036fd43afeca4b36d0a
Minotaurius <brandonslanger@gmail.com>
2022-08-18 20:12:10 -0500

    First 3 category routes working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,37 +2,55 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
   // find all categories
   Category.findAll({
     include: {
       model: Product,
-      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
       // be sure to include its associated Products
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
     }
   })
-  .then((catData) => {
-    res.json(catData)
-    if(err) console.log(err);
-  })
+    .then((catData) => {
+      res.json(catData);
+    })
+    .catch(err => {
+      console.log(err)
+    });
 });
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  Category.findOne({ where: { id: req.params.id }, 
-    include:[{ model: Product,
-    attributes:['id', 'product_name', 'price', 'stock', 'category_id']}]
-    })
-  .then((catData) => {
-    res.json(catData)
-    if(err) console.log(err);
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      // be sure to include its associated Products
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
   })
-  // be sure to include its associated Products
+    .then((idCat) => {
+      res.json(idCat);
+    })
+    .catch(err => {
+      console.log(err)
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then((newCat) => {
+    res.json(newCat);
+  })
+  .catch(err => {
+    console.log(err)
+  });
 });
 
 router.put('/:id', (req, res) => {
